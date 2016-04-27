00639986cda2d40afddc529d3defe8839a96a3c9
Andrew Efimenko <anrybio@gmail.com>
2016-04-27 12:16:51 +0200

    Поправил названия

--- platform/android/com/appmetrica/cordova/plugin/AppMetrica.java
+++ platform/android/com/appmetrica/cordova/plugin/AppMetrica.java
@@ -15,15 +15,18 @@ import com.yandex.metrica.YandexMetrica;
 import android.content.Context;
 import android.util.Log;
 
-public class AppMetricaPlugin extends CordovaPlugin { 
+public class AppMetrica extends CordovaPlugin {
     @Override
     public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if("activate".equals(action))
-        {
-            activate(args, callbackContext);
-            return true;
-        }
-        return false;
+  		if("activate".equals(action))
+      {
+          activate(args, callbackContext);
+          return true;
+      }
+      if("reportEvent".equals(action)){
+        reportEvent(args, callbackContext);
+      }
+      return false;
     }
 
     @Override
@@ -52,6 +55,26 @@ public class AppMetricaPlugin extends CordovaPlugin {
                         YandexMetrica.activate(cordova.getActivity().getApplicationContext(), devKey);
                         YandexMetrica.onResumeActivity(cordova.getActivity());
                         YandexMetrica.reportEvent("activated");
+					}
+				});
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void reportEvent(JSONArray parameters, final CallbackContext callbackContext) {
+        try
+        {
+            final String eventName = parameters.getString(0);
+            if(eventName != null){
+				cordova.getThreadPool().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        YandexMetrica.reportEvent(eventName);
 					}
 				});
             }
