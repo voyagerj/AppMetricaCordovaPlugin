dafefb3b6a5b7471f2f05fc9f3256349d799341d
Andrew Andy Yudkin <Andy@Andrews-iMac.local>
2016-07-06 10:47:12 +0200

    update js file

--- www/AppMetrica.js
+++ www/AppMetrica.js
@@ -1,41 +1,38 @@
-cordova.define("com.appmetrica.cordova.plugins.appmetrica.AppMetrica", function(require, exports, module) {
-    (function (global) {
-        var AppMetrica;
-        AppMetrica = function () {
+(function (global) {
+    var AppMetrica;
+    AppMetrica = function () {
+    };
+
+    AppMetrica.prototype.activate = function (args) {
+        cordova.exec(function(winParam) {},
+            function(error) {
+                console.warn('cordova.exec ' + error)
+            },
+            "AppMetrica",
+            "activate",
+            [args]);
+    };
+
+    AppMetrica.prototype.reportEvent = function (event, params) {
+        cordova.exec(function(winParam) {},
+            function(error) {
+                console.warn('cordova.exec ' + error)
+            },
+            "AppMetrica",
+            "reportEvent",
+            [event, params]);
+    };
+
+
+    global.cordova.addConstructor(function() {
+        if (!global.Cordova) {
+            global.Cordova = global.cordova;
         };
 
-        AppMetrica.prototype.activate = function (args) {
-            cordova.exec(function(winParam) {},
-                function(error) {
-                    console.warn('cordova.exec ' + error)
-                },
-                "AppMetrica",
-                "activate",
-                [args]);
-        };
+        if (!global.plugins) {
+            global.plugins = {};
+        }
 
-        AppMetrica.prototype.reportEvent = function (event, params) {
-            cordova.exec(function(winParam) {},
-                function(error) {
-                    console.warn('cordova.exec ' + error)
-                },
-                "AppMetrica",
-                "reportEvent",
-                [event, params]);
-        };
-
-
-        global.cordova.addConstructor(function() {
-            if (!global.Cordova) {
-                global.Cordova = global.cordova;
-            };
-
-            if (!global.plugins) {
-                global.plugins = {};
-            }
-
-            global.plugins.appMetrica = new AppMetrica();
-        });
-    }(window));
-
-});
+        global.plugins.appMetrica = new AppMetrica();
+    });
+}(window));
